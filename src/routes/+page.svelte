951187b64ea4cca55f0e32faacd84c1f08e4dcef
unyamma <ppcrosout@gmail.com>
2024-02-25 15:09:23 +0100

    Badge created : my contact

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="badge badge-secondary badge-outline rounded-full h-9 w-24">
+  <div class="join">
+    <div class="avatar">
+      <div class="w-7 h-7 self-center rounded-full m-0.5 bg-secondary">
+        <img src="https://i.imgur.com/vXLunlv.png" alt="" />
+      </div>
+    </div>
+    <div class="badge badge-accent h-7 h-3/4 m-0.5 rounded-full">@spfj</div>
+  </div>
+</div>
